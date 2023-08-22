ceab56e1eb1de92db0ff04b4baf62d1984e44050
Edmund Ju <edmundju@berkeley.edu>
2023-08-21 19:01:40 -0700

    initial terraform

--- main.tf
+++ main.tf
@@ -9,3 +9,27 @@ resource "aws_instance" "example_instance" {
         Name = "ExampleInstance"
     }
 }
+
+resource "aws_cloudwatch_metric_alarm" "example_alarm" {
+    alarm_name = "ExampleAlarm"
+    comparison_operator = "GreaterThanOrEqualToThreshold"
+    evaluation_periods = "1"
+    metric_name = "CPUUtilization"
+    namespace = "AWS/EC2"
+    period = "300"
+    statistic = "Average"
+    threshold = "70"
+    alarm_description = "This metric checks for CPU utilization higher than 70%"
+    alarm_actions = [aws_instance.example_instance.id]
+
+    dimensions = {
+        InstanceId = aws_instance.example_instance.id
+    }
+
+    treat_missing_data = "notBreaching"
+
+}
+
+output "instance_id" {
+    value = aws_instance.example_instance.id
+}
